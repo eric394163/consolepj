d089f0573bce327faa7e1a5a127dbbedce9c1489
dasom1007 <nab1kong@naver.com>
2024-01-11 18:01:35 +0900

    -

---
+++ Personal_folder/DaSom/AccountBook/homework3/UpdateIncomeExpenseManager.java
@@ -0,0 +1,42 @@
+package homework3;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import homework3.InputIncomeExpenseInterface.Transaction;
+
+
+public class UpdateIncomeExpenseManager implements UpdateIncomeExpenseInterface {
+	
+		@Override
+		public void UpdateIncomeExpenseRun() {
+			
+	        Scanner scan = new Scanner(System.in);
+	        List<Transaction> transactions = new ArrayList<>();
+
+	        System.out.print("새로운 수입(+)/지출(-): ");
+			char type = scan.next().charAt(0);
+
+	        System.out.print("새로운 금액: ");
+	        double amount = scan.nextDouble();
+	        scan.nextLine(); // 버퍼 비우기
+
+	        System.out.print("새로운 날짜 (yyyy-MM-dd): ");
+	        String date = scan.nextLine();
+
+	        System.out.print("새로운 내역 입력: ");
+	        String category = scan.nextLine();
+	        
+	        //Transaction transaction = new Transaction();
+	    }
+
+		@Override
+		public void modifyTransaction(Transaction transaction) {}
+		
+	}
+	
+      
+    
+
