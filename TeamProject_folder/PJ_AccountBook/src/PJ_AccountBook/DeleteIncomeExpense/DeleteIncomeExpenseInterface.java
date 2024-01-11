1cada1b2e90437f22fb2f467ed88cd6f3080133e
KIM SUNG HYUN <123492644+eric394163@users.noreply.github.com>
2024-01-11 19:23:14 +0900

    Delete TeamProject_folder directory

--- TeamProject_folder/PJ_AccountBook/src/PJ_AccountBook/DeleteIncomeExpense/DeleteIncomeExpenseInterface.java
+++
@@ -1,9 +0,0 @@
-package PJ_AccountBook.DeleteIncomeExpense;
-
-public interface DeleteIncomeExpenseInterface {
-    void DeleteIncomeExpenseRun();
-
-    void displayAccountBook();
-
-    void deleteIncomeOrExpense();
-}
